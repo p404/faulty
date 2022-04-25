40b7e33dd31f7511d2b126efa94169721fc47917
Pablo Opazo <pablo@sequel.ninja>
2022-04-25 14:25:34 -0700

    Added instrumentation

--- go.mod
+++ go.mod
@@ -2,6 +2,22 @@ module github.com/p404/faulty
 
 go 1.17
 
-require github.com/github/go-fault v0.2.0
+require (
+	github.com/github/go-fault v0.2.0
+	github.com/gorilla/mux v1.8.0
+	go.opentelemetry.io/contrib/propagators/b3 v1.6.0
+	go.opentelemetry.io/otel v1.6.3
+	go.opentelemetry.io/otel/exporters/jaeger v1.6.3
+	go.opentelemetry.io/otel/sdk v1.6.3
+)
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/felixge/httpsnoop v1.0.2 // indirect
+	github.com/go-logr/logr v1.2.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.30.0 // indirect
+	go.opentelemetry.io/otel/internal/metric v0.27.0 // indirect
+	go.opentelemetry.io/otel/metric v0.27.0 // indirect
+	go.opentelemetry.io/otel/trace v1.6.3 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
+)
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
@@ -10,7 +11,16 @@ import (
 
 	"github.com/github/go-fault"
 	"github.com/gorilla/mux"
+	"go.opentelemetry.io/contrib/propagators/b3"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/jaeger"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
+
+const serviceName = "faulty"
 
 var (
 	externalService = flag.String("microservice", "http://www.google.com", "Microservice Endpoint")
@@ -18,10 +28,47 @@ var (
 	percent         = flag.Float64("percent", 0, "Percentage of faults")
 	faultType       = flag.String("type", "error", "Type of the value")
 	latency         = flag.Int64("latency", 1, "Latency in milliseconds to be added to fault type slowness")
+	collectorAddr   = flag.String("collector", "http://localhost:14268/api/traces", "Collector endpoint")
 )
 
 func init() {
 	flag.Parse()
+}
+
+func setupTracing() func(context.Context) {
+	tp, err := tracerProvider(*collectorAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	otel.SetTracerProvider(tp)
+	otel.SetTextMapPropagator(propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+		b3.New(),
+	))
+
+	return func(ctx context.Context) {
+		ctx, cancel := context.WithTimeout(ctx, time.Second*5)
+		defer cancel()
+		if err := tp.Shutdown(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
+	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
+	if err != nil {
+		return nil, err
+	}
+	tp := tracesdk.NewTracerProvider(
+		tracesdk.WithBatcher(exp),
+		tracesdk.WithResource(resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String(serviceName),
+		)),
+	)
+	return tp, nil
 }
 
 func handleAuthz(w http.ResponseWriter, r *http.Request) {
@@ -56,6 +103,12 @@ func main() {
 	} else if strings.TrimRight(*faultType, "\n") == "slowness" {
 		errorInjector, _ = fault.NewSlowInjector(time.Millisecond * latencyDuration)
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	shutdown := setupTracing()
+	defer shutdown(ctx)
 
 	errorFault, _ := fault.NewFault(errorInjector,
 		fault.WithEnabled(*faults),
